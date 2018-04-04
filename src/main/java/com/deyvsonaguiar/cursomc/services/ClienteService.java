1c5467b0bf867faa15f7c012a2cfb5da4f3f2b21
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2018-04-03 21:14:42 -0400

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/deyvsonaguiar/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.deyvsonaguiar.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.deyvsonaguiar.cursomc.domain.Cliente;
+import com.deyvsonaguiar.cursomc.repositories.ClienteRepository;
+import com.deyvsonaguiar.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
