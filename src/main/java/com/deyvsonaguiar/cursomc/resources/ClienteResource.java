ed55e607ab4488e58f8fbd7c5a8ae7219732d2e1
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2018-05-12 18:03:53 -0400

    Inserindo um novo Cliente com Post

--- src/main/java/com/deyvsonaguiar/cursomc/resources/ClienteResource.java
+++ src/main/java/com/deyvsonaguiar/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.deyvsonaguiar.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,8 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.deyvsonaguiar.cursomc.domain.Cliente;
 import com.deyvsonaguiar.cursomc.dto.ClienteDTO;
+import com.deyvsonaguiar.cursomc.dto.ClienteNewDTO;
 import com.deyvsonaguiar.cursomc.services.ClienteService;
 
 @RestController
@@ -24,6 +28,15 @@ public class ClienteResource {
 	
 	@Autowired
 	private ClienteService service;
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 	
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id)
--- src/main/java/com/deyvsonaguiar/cursomc/services/ClienteService.java
+++ src/main/java/com/deyvsonaguiar/cursomc/services/ClienteService.java
@@ -10,9 +10,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.deyvsonaguiar.cursomc.domain.Cidade;
 import com.deyvsonaguiar.cursomc.domain.Cliente;
+import com.deyvsonaguiar.cursomc.domain.Endereco;
+import com.deyvsonaguiar.cursomc.domain.enums.TipoCliente;
 import com.deyvsonaguiar.cursomc.dto.ClienteDTO;
+import com.deyvsonaguiar.cursomc.dto.ClienteNewDTO;
 import com.deyvsonaguiar.cursomc.repositories.ClienteRepository;
+import com.deyvsonaguiar.cursomc.repositories.EnderecoRepository;
 import com.deyvsonaguiar.cursomc.services.exceptions.DataIntegrityException;
 import com.deyvsonaguiar.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +26,16 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj; 
+	}
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -55,6 +70,21 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.ToEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
