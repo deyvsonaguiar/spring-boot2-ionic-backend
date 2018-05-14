fd7fbee2ac51ae0093b4a0771975065c0b62eb50
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2018-05-13 23:48:35 -0400

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/deyvsonaguiar/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/deyvsonaguiar/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.deyvsonaguiar.cursomc.domain.Cliente;
+import com.deyvsonaguiar.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
