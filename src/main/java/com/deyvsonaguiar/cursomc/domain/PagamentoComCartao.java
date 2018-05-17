7a131380756a0481446034c0b65aff64a216cb2a
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2018-05-17 17:47:01 -0400

    Inserindo Pedidos

--- src/main/java/com/deyvsonaguiar/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/deyvsonaguiar/cursomc/domain/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.deyvsonaguiar.cursomc.domain;
 import javax.persistence.Entity;
 
 import com.deyvsonaguiar.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
