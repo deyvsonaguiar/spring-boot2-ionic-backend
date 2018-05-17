3dbfe54ae1f3e4df18b0ef2002bfb37153183766
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2018-05-17 16:25:39 -0400

    Busca de produtos por nome e categorias

--- src/main/java/com/deyvsonaguiar/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/deyvsonaguiar/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,23 @@
 package com.deyvsonaguiar.cursomc.repositories;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.deyvsonaguiar.cursomc.domain.Categoria;
 import com.deyvsonaguiar.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+	@Transactional
+	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN:categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+	//Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categoria, Pageable pageRequest);
 
 }
--- src/main/java/com/deyvsonaguiar/cursomc/services/validation/ClienteUpdate.java
+++ src/main/java/com/deyvsonaguiar/cursomc/services/validation/ClienteUpdate.java
@@ -8,8 +8,6 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-import com.deyvsonaguiar.cursomc.services.validation.ClienteInsertValidator;
-
 @Constraint(validatedBy = ClienteUpdateValidator.class)
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
