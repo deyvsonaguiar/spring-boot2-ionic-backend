eecdc7655dbe65342ab3271d6d19113bc8065246
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2018-05-17 16:34:37 -0400

    Subtotal e total do pedido

--- src/main/java/com/deyvsonaguiar/cursomc/domain/ItemPedido.java
+++ src/main/java/com/deyvsonaguiar/cursomc/domain/ItemPedido.java
@@ -28,6 +28,11 @@ public class ItemPedido {
 		this.preco = preco;
 	}
 	
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+	
+	
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
